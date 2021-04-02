aebc98627525ede56f218185f0e1b46867949364
Phillip O'Reggio <pnoreggio@gmail.com>
2021-04-02 16:37:38 -0400

    Get Xcode

---
+++ SwiftUIGraphQLDemo/SwiftUIGraphQLDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIGraphQLDemo
+//
+//  Created by Phillip OReggio on 4/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGraphQLDemo/SwiftUIGraphQLDemo/SwiftUIGraphQLDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGraphQLDemoApp.swift
+//  SwiftUIGraphQLDemo
+//
+//  Created by Phillip OReggio on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGraphQLDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
